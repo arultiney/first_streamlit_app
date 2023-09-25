dab40553c8db2e8af283746d6408e625646796ee
arultiney <145861592+arultiney@users.noreply.github.com>
2023-09-25 23:49:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
+stremalit.Title('My Parents healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
